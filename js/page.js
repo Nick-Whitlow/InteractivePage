4f735f6cdaf573bd64ad9192d1e15eda80286e0c
Nick Whitlow <nickwhitlow@Gracies-MacBook-Air.local>
2020-10-26 13:57:21 -0400

    Added correct answer

--- js/page.js
+++ js/page.js
@@ -3,3 +3,17 @@ d3.select("#Spelling")
 d3.select("#questionList")
     .classed("hidden", false)
 })
+
+d3.select(".checkAnswer") 
+    .on("click", function(){
+    d3.selectAll("correct")
+    .attr("class", "markCorrect")
+        })
+
+  d3.select(".reset")  
+    .on("click",function()
+    {
+   d3.selectAll("correct")
+    .attr("class", "normal")
+})
+
